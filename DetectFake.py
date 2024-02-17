538be1f39010db3b37ebc00f562d1322b18b0db8
wuyiulin <wuyiulin@gmail.com>
2024-02-17 15:08:14 +0800

    update our DCT method

--- DetectFake.py
+++ DetectFake.py
@@ -18,7 +18,7 @@ def timeViewer(func):
         return result
     return inner
 
-def DCT(block, dict, N):
+def numpyDCT(block, dict, N):
     block = np.float32(block)
     dct = cv2.dct(block)
     for i in range(N):
@@ -26,29 +26,112 @@ def DCT(block, dict, N):
             strDij = str(abs(dct[i,j]))[0]
             if(strDij!='0'):
                 dict[strDij]+=1
-    # para = 1/(2*np.sqrt(2*N))
-    # OrthogonalValue = 1/np.sqrt(2)
-    # for i in range(N):
-    #     for j in range(N):
-    #         if(i==0):
-    #             Ci = OrthogonalValue
-    #         else:
-    #             Ci = 1
-    #         if(j==0):
-    #             Cj = OrthogonalValue
-    #         else:
-    #             Cj = 1
 
-    #         Dij = 0
-    #         for x in range(N):
-    #             for y in range(N):
-    #                 Dij += para*Ci*Cj*(block[x,y]) * np.cos((2*x+1)*i*np.pi / (2*N)) * np.cos((2*y+1)*j*np.pi / (2*N))
-    #         strDij = str(abs(Dij))[0]
-    #         if(strDij!='0'):
-    #             dict[strDij]+=1
+def oldDCT(block, dict, N):
+    para = 1/(2*np.sqrt(2*N))
+    OrthogonalValue = 1/np.sqrt(2)
+    for i in range(N):
+        for j in range(N):
+            if(i==0):
+                Ci = OrthogonalValue
+            else:
+                Ci = 1
+            if(j==0):
+                Cj = OrthogonalValue
+            else:
+                Cj = 1
+
+            Dij = 0
+            for x in range(N):
+                for y in range(N):
+                    Dij += para * Ci * Cj * (block[x,y]) * np.cos((2*x+1)*i*np.pi / (2*N)) * np.cos((2*y+1)*j*np.pi / (2*N))
+            strDij = str(abs(Dij))[0]
+            if(strDij!='0'):
+                dict[strDij]+=1
+
+def genrateDCTmask(N):
+    mask = np.zeros((N, N), dtype=np.float32)
+    para = 1/(2*np.sqrt(2*N))
+    OrthogonalValue = 1/np.sqrt(2)
+    for i in range(N):
+        for j in range(N):
+            if(i==0):
+                Ci = OrthogonalValue
+            else:
+                Ci = 1
+            if(j==0):
+                Cj = OrthogonalValue
+            else:
+                Cj = 1
+
+            for x in range(N):
+                for y in range(N):
+                    mask[i,j] += para * Ci * Cj * np.cos((2*x+1)*i*np.pi / (2*N)) * np.cos((2*y+1)*j*np.pi / (2*N))
+    return mask
+
+def MutiDCT(shared_data, DCTmask, block, lock, N):
+    Sdata = np.frombuffer(shared_data.get_obj(), dtype=np.float32)
+    localArr = np.zeros(9, dtype=np.float32)
+    DCT = np.dot(DCTmask, block)
+
+    for i in range(N):
+        for j in range(N):
+            strDij = str(abs(DCT[i,j]))[0]
+            if(strDij!='0'):
+                Dij = int(strDij)
+                localArr[Dij-1]+=1
+
+    with lock:
+        Sdata += localArr
+
+    
+
 
 @timeViewer
-def SingleTransform(img):
+def oldSingleTransform(img):
+    print("w/o Mask method.")
+    # 初始化圖片的首位字典
+    manager = multiprocessing.Manager()
+    dict = manager.dict({str(i): 0 for i in range(1, 10)})
+
+    # 初始化 Benford's Law 的標準字典
+    BenfordsDict = {str(i): np.log10((i+1)/i)  for i in range(1, 10)}
+
+    # 設定區塊大小
+    N = 8
+
+    # 將圖片分成可以被 8x8 整除的尺寸
+    h, w = img.shape
+    hPara = h // N
+    wPara = w // N
+    img = cv2.resize(img, (wPara*N, hPara*N))
+
+
+    # 將圖片分成 8x8 的區塊
+    blocks = [np.float32(img[i:i+N, j:j+N]-128) for i in range(0, img.shape[0], N) for j in range(0, img.shape[1], N)]
+
+    # 算 DCT 並紀錄首位數字
+    times = len(blocks)
+    progress = tqdm(total=times)
+    for block in blocks:
+        oldDCT(block, dict, N)
+        progress.update(1)
+    
+    # 計算結果
+    result = 0
+    SumValue = img.shape[0] * img.shape[1]
+    # print("Single Dict:")
+    # print(dict)
+    # print("Single SumValue: "+ str(SumValue))
+    for key in dict:
+        dict[key] /= SumValue
+        result += abs(BenfordsDict[key] - dict[key])
+    
+    return result
+
+
+@timeViewer
+def numpySingleTransform(img):
 
     # 初始化圖片的首位字典
     manager = multiprocessing.Manager()
@@ -74,11 +157,9 @@ def SingleTransform(img):
     times = len(blocks)
     progress = tqdm(total=times)
     for block in blocks:
-        DCT(block, dict, N)
+        numpyDCT(block, dict, N)
         progress.update(1)
-        
 
-    
     # 計算結果
     result = 0
     SumValue = img.shape[0] * img.shape[1]
@@ -90,6 +171,59 @@ def SingleTransform(img):
         result += abs(BenfordsDict[key] - dict[key])
     
     return result
+
+
+@timeViewer
+def SingleTransform(img):
+    print("w Mask method.")
+    # 初始化圖片的首位字典
+    manager = multiprocessing.Manager()
+    dict = manager.dict({str(i): 0 for i in range(1, 10)})
+
+    # 初始化 Benford's Law 的標準字典
+    BenfordsDict = {str(i): np.log10((i+1)/i)  for i in range(1, 10)}
+
+    # 設定區塊大小
+    N = 8
+
+    # 初始化 DCTmask
+    DCTmask = genrateDCTmask(N)
+
+    # 將圖片分成可以被 8x8 整除的尺寸
+    h, w = img.shape
+    hPara = h // N
+    wPara = w // N
+    img = cv2.resize(img, (wPara*N, hPara*N))
+
+
+    # 將圖片分成 8x8 的區塊
+    blocks = [np.float32(img[i:i+N, j:j+N]-128) for i in range(0, img.shape[0], N) for j in range(0, img.shape[1], N)]
+
+    # 算 DCT 並紀錄首位數字
+    times = len(blocks)
+    progress = tqdm(total=times)
+    
+    for block in blocks:
+        block = np.dot(block, DCTmask)
+        for i in range(N):
+            for j in range(N):            
+                strDij = str(abs(block[i,j]))[0]
+                if(strDij!='0'):
+                    dict[strDij]+=1
+        progress.update(1)
+
+    # 計算結果
+    result = 0
+    SumValue = img.shape[0] * img.shape[1]
+    # print("Single Dict:")
+    # print(dict)
+    # print("Single SumValue: "+ str(SumValue))
+    for key in dict:
+        dict[key] /= SumValue
+        result += abs(BenfordsDict[key] - dict[key])
+    
+    return result
+
 
 @timeViewer
 def MultiTransform(img, cores=None):
@@ -104,48 +238,38 @@ def MultiTransform(img, cores=None):
     # 將圖像分成 8x8 的區塊
     blocks = [np.float32(img[i:i+N, j:j+N]-128) for i in range(0, img.shape[0], N) for j in range(0, img.shape[1], N)]
 
-    # 初始化圖片的首位字典
-    manager = multiprocessing.Manager()
-    dict = manager.dict({str(i): 0 for i in range(1, 10)})
+    # 宣告共享的記憶體
+    shared_data = multiprocessing.Array('i', 9)
+    for i in range(len(shared_data)):
+        shared_data[i] = 0  # 初始化所有值為0
 
-    # 初始化 Benford's Law 的標準字典
-    BenfordsDict = {str(i): np.log10((i+1)/i)  for i in range(1, 10)}
+    # 初始化 Benford's Law 的標準 Array
+    BenfordsArray = np.array([np.log10((i+1)/i) for i in range(1, 10)])
 
-    ## 算 DCT 並紀錄首位數字
-    times = len(blocks)
-    progress = tqdm(total=times)
-
-    # 建立多進程的進程池
-    pool = multiprocessing.Pool(processes=num_cores)
-
-    # 對每個 block 啟動一個進程處理
+    # 多 process 處理 DCT 運算
+    DCTmask = genrateDCTmask(N)
+    lock = multiprocessing.Lock()
+    processes = []
+    num_processes = len(blocks)
+    progress = tqdm(total=num_processes)
     for block in blocks:
-        pool.apply_async(DCT, args=(block, dict, N))
+        p = multiprocessing.Process(target=MutiDCT, args=(shared_data, DCTmask, block, lock, N))
+        processes.append(p)
+        p.start()
         progress.update(1)
-    
-    # 等待所有進程完成
-    pool.close()
-    pool.join()
+
+    for p in processes:
+        p.join()
     
     # 計算結果
     result = 0
-    SumValue = sum(dict.values())
-    for key in dict:
-        dict[key] /= SumValue
-        result += abs(BenfordsDict[key] - dict[key])
+    SumValue = np.sum(shared_data)
+    result_data = shared_data / SumValue
+    result_data = abs(result_data - BenfordsArray)
+    result = np.sum(result_data)
     print("SumValue: " + str(SumValue))
-    print(dict)
+    print(shared_data)
 
-    # # 將字典的值轉換為列表
-    # observed = list(dict.values())
-    # expected = list(BenfordsDict.values())
-
-    # # 將列表轉換為 numpy array
-    # observed_array = np.array(observed)
-    # expected_array = np.array(expected)
-    # # 進行卡方檢定
-    # chi2_stat, p_val = chisquare(observed_array, f_exp=expected_array)
-    # print("卡方檢定值: " + str('{:.3f}'.format(chi2_stat)) + ", P值: " + str('{:.3f}'.format(p_val)))
     return result
 
 
@@ -154,11 +278,15 @@ def MultiTransform(img, cores=None):
 if __name__ == '__main__':
 
     image = cv2.imread('./Head.jpg', cv2.IMREAD_GRAYSCALE)
-    # resultS = SingleTransform(image)
-    resultM = MultiTransform(image)
+    resultO = oldSingleTransform(image)
+    # resultS = numpySingleTransform(image)
+    resultF = SingleTransform(image)
+    # resultM = MultiTransform(image)
 
+    # print("這張照片的修圖程度(單執行緒)： " + str('{:.3f}'.format(resultO)) +"\n")
     # print("這張照片的修圖程度(單執行緒)： " + str('{:.3f}'.format(resultS)) +"\n")
-    print("這張照片的修圖程度(多執行緒)： " + str('{:.3f}'.format(resultM)) +"\n")
+    # print("這張照片的修圖程度(單執行緒)： " + str('{:.3f}'.format(resultF)) +"\n")
+    # print("這張照片的修圖程度(多執行緒)： " + str('{:.3f}'.format(resultM)) +"\n")
 
 
 
