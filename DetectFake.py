78bfd7227c243a82ba609d28bf7ddbee83fb2826
wuyiulin <wuyiulin@gmail.com>
2024-02-18 21:29:04 +0800

    update comment

--- DetectFake.py
+++ DetectFake.py
@@ -70,14 +70,23 @@ def genrateDCTmask(N):
                 for y in range(N):
                     mask[i, j, x, y] = (para * Ci * Cj * np.cos((2*x+1)*i*np.pi / (2*N)) * np.cos((2*y+1)*j*np.pi / (2*N)))
                     # print("Now i j x y:" + str(i) + " " + str(j) + " " + str(x) + " " + str(y) + " ")
-                    # pdb.set_trace()
+    
+    minimum = (np.min(abs(mask)))
+    AMPfactor = 1
+    
+    while(minimum < 1):
+        minimum *= 10
+        AMPfactor *=10
+
+    mask *= AMPfactor
+
     return mask
 
 def dotMask(block, mask, dict, N):
     DCT = np.zeros((N,N), dtype=np.float32)
     for i in range(N):
         for j in range(N):
-            DCT[i, j] = np.sum(block * mask[i, j]*100)
+            DCT[i, j] = np.sum(block * mask[i, j])
             strDij = str(abs(DCT[i,j]))[0]
             # print("i: " + str(i) + ", j: " + str(j))
             # pdb.set_trace()
@@ -94,7 +103,7 @@ def MutiDCT(args):
     
     for i in range(N):
         for j in range(N):
-            DCT[i, j] = np.sum(block * mask[i, j]*100)
+            DCT[i, j] = np.sum(block * mask[i, j])
             strDij = str(abs(DCT[i,j]))[0]
             if strDij != '0':
                 Dij = int(strDij)
@@ -134,7 +143,7 @@ def oldSingleTransform(img):
     times = len(blocks)
     progress = tqdm(total=times)
     for block in blocks:
-        oldDCT(block, dict, N)
+        oldDCT(block*1000, dict, N)
         progress.update(1)
     
     # 計算結果
@@ -145,7 +154,7 @@ def oldSingleTransform(img):
     # print("Single SumValue: "+ str(SumValue))
     for key in dict:
         dict[key] /= SumValue
-        result += abs(BenfordsDict[key] - dict[key])
+        result += abs(BenfordsDict[key] - dict[key]) * (1 / BenfordsDict[key])
     
     return result
 
@@ -176,22 +185,22 @@ def OpenCVSingleTransform(img):
     times = len(blocks)
     progress = tqdm(total=times)
     for block in blocks:
-        OpenCVDCT(block, dict, N)
+        OpenCVDCT(block*1000, dict, N)
         progress.update(1)
 
     # 計算結果
     result = 0
     SumValue = img.shape[0] * img.shape[1]
-    print("Single Dict:")
-    print(dict)
-    print("Single SumValue: "+ str(SumValue))
+    # print("Single Dict:")
+    # print(dict)
+    # print("Single SumValue: "+ str(SumValue))
     for key in dict:
         dict[key] /= SumValue
         result += abs(BenfordsDict[key] - dict[key]) * (1 / BenfordsDict[key])
-    print("BenfordsDict: ")
-    print(BenfordsDict)
-    print("OurDict: ")
-    print(dict)
+    # print("BenfordsDict: ")
+    # print(BenfordsDict)
+    # print("OurDict: ")
+    # print(dict)
 
     return result
 
@@ -286,15 +295,15 @@ if __name__ == '__main__':
 
     image = cv2.imread('./Head.jpg', cv2.IMREAD_GRAYSCALE)
     # image = cv2.resize(image, (512, 512), interpolation=cv2.INTER_NEAREST)
-    # resultO = oldSingleTransform(image)
-    # resultS = OpenCVSingleTransform(image)
+    resultO = oldSingleTransform(image)
+    resultS = OpenCVSingleTransform(image)
     resultF = SingleTransform(image)
     resultM = MultiTransform(image)
 
-    # print("這張照片的修圖程度(單執行緒)： " + str('{:.3f}'.format(resultO)) +"\n")
-    # print("這張照片的修圖程度(單執行緒)： " + str('{:.3f}'.format(resultS)) +"\n")
-    print("這張照片的修圖程度(單執行緒)： " + str('{:.3f}'.format(resultF)) +"\n")
-    print("這張照片的修圖程度(多執行緒)： " + str('{:.3f}'.format(resultM)) +"\n")
+    print("這張照片的修圖程度(單執行緒 土法煉鋼 Method)： " + str('{:.3f}'.format(resultO)) +"\n")
+    print("這張照片的修圖程度(單執行緒 OpenCV  Method)： " + str('{:.3f}'.format(resultS)) +"\n")
+    print("這張照片的修圖程度(單執行緒 Mask    Method)： " + str('{:.3f}'.format(resultF)) +"\n")
+    print("這張照片的修圖程度(多執行緒 Mask    Method)： " + str('{:.3f}'.format(resultM)) +"\n")
 
 
 
